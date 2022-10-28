3ac28e983e1bb84290f32ed7d5eb2df413dabcd8
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2022-10-28 13:22:11 +0000

    Tests are all working

--- src/evercraft/evercraft.py
+++ src/evercraft/evercraft.py
@@ -35,33 +35,77 @@
 
 
 #   Character Ability Scores (Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma)
+#       a list of variables and each variable will be set to a default of 10 that can be modifed in the class
+#       once a new number is passed into it, 
+#       loop through each variable and set it equal to an integer and then pass that into a function to determine the modifier
+#       
+#
 
 
 #   Exp points (When a successful attack occurs, the character gains 10 experience points)
+#             
+#
+#
+#
 
 
 #   Level Scaling (1000Xp = new level || hit points increase by 5 plus Con modifier || 1 is added to roll for every even level achieved (Ex: level 2 adds 1 to roll
 #                                                                                                                                          if roll is 14 + 1)
 #------------------------------------------------------------------------
 
+# class setAbility():
+    # def __init__(self, Type, Num):
+    #     for Ability in Character.Skills:
+    #         if Ability == Type:
+    #             Abilities(Type, Num)
+                
+
+# class Abilities():
+#     def __init__(self, Type, Num):
+#         self.Type = Num
+
+# class Abilities():
+    # def __init__(self):
+    #     self.Strength = 10
+    #     self.Dexterity = 10
+    #     self.Constitution = 10
+    #     self.Wisdom = 10
+    #     self.Intelligence = 10
+    #     self.Charisma = 10
+from evercraft.mod import modify
 
 class Character():
     def __init__(self, name, alignment):
+        self.skill =  {
+            "Strength" : 10,
+            "Dexterity" : 10,
+            "Constitution" : 10,
+            "Wisdom" : 10,
+            "Intelligence" : 10, 
+            "Charisma" : 10
+        }      
         self.name = name
         self.alignment = alignment
-        self.armor = 10
         self.HP = 5 
         self.isdead = False
+        self.armor = 10
+        self.xp = 1000
     def attack(self, roll, enemy):
-        DP = 1
+        bonus = modify(self.skill["Strength"])
+        roll = roll + bonus
         if roll < enemy.armor:
             DP = 0
         if roll >= enemy.armor:
-            DP = 1
-        if roll == 20:
-            DP = DP * 2
+            DP = 1 + bonus
+        if (roll - bonus) == 20:
+            DP = DP * 2 
         enemy.damage(DP)
         return DP
     def damage(self, DP):
         self.HP = self.HP - DP
+        if self.HP <= 0:
+            self.isdead = True
 
+    # class Fighter(Character):
+    #     def __init__ (self):#we only want to affect child NOT parent
+    #         super().__init__()#need to also access the init method of parent, where super fn comes in
